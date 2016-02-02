50d6ac869f463933dd0a0b2e03ce4bf84447472c
kpjonsson <philip.jonsson@gmail.com>
2016-02-01 21:33:21 -0500

    Added many_colors.R

---
+++ R/add_hotspots_to_maf.R
@@ -0,0 +1,14 @@
+### List of hotspot alleles generated thusly:
+# hotspots = read_tsv('~/Desktop/Misc/hotspot.v11.mutations.filtered.txt') %>% filter(judgement == 'RETAIN') %>% mutate(var = strsplit(var, '\\|')) %>%
+#     tidyr::unnest(var) %>% select(gene, ref, var, aa_position, judgement) %>% mutate(ref = str_replace(ref, ':[0-9]*', '')) %>% mutate(var = str_replace(var, ':[0-9]*', '')) %>%
+#     mutate(mutation = str_c('p.', ref, aa_position, var)) %>% select(Hugo_Symbol = gene, HGVSp_Short = mutation) %>% mutate(Hotspot = TRUE)
+# write.table(hotspots, file = 'data/hotspots.txt', col.names = T, row.names = F, sep = '\t', quote = F)
+
+add_hotspots_to_maf = function(maf) {
+    data(hotspots)
+    maf = merge(maf, hotspots, by = c('Hugo_Symbol', 'HGVSp_Short'), all.x = TRUE)
+    maf$Hotspot[is.na(maf$Hotspot)] = FALSE
+    maf
+}
+
+
---
+++ R/rownum.R
@@ -0,0 +1,9 @@
+rownum = function(df) {
+
+	if(class(df) %in% c('data.frame', 'data.matrix')) {
+		rownames(df) = seq(1, nrow(df))
+		return(df)
+	}
+	else { print('Wrong input type') }
+
+}
