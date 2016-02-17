d96e00e8ebde21961ab51cc7355c81672ecb159a
kpjonsson <philip.jonsson@gmail.com>
2016-02-17 13:56:09 -0500

    Changes to theme_bwmin

--- R/theme_bwmin.R
+++ R/theme_bwmin.R
@@ -2,14 +2,24 @@
 #'
 #' Use by adding \code{+theme_bwmin()} to your ggplot call.
 
-theme_bwmin = function(base_size = 12, base_family = 'Helvetica') {
-  theme_bw(base_size = base_size, base_family = base_family) %+replace%
+theme_bwmin = function(
+    base_size = 10,
+    base_family = 'Helvetica',
+    ticks = TRUE)
+{
+  ret = theme_bw(base_size = base_size, base_family = base_family) +
     theme(
-          panel.grid.major = element_blank(),
-          panel.border = element_rect(fill = NA, colour = 'black', size = 0.5),
-          panel.grid.minor = element_blank(),
-          strip.background = element_rect(fill = NA, colour = NA),
-          axis.line = element_blank(),
-          axis.ticks = element_line(size = 0.5)
-          )
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        strip.background = element_blank(),
+        axis.line = element_blank(),
+        legend.background = element_blank(),
+        legend.key = element_blank(),
+        legend.key.size = unit(.25, "cm"),
+        panel.border = element_rect(fill = NA, colour = 'black', size = 0.5),
+        axis.ticks = element_line(size = 0.5)
+        )
+  if (!ticks) ret = ret + theme(axis.ticks = element_blank())
+  ret
 }
