eb3230bad6e8955001b824c88b09fafa41a69ac5
kpjonsson <philip.jonsson@gmail.com>
2016-08-16 08:38:17 -0400

    Fix

--- R/add_hotspots_to_maf.R
+++ R/add_hotspots_to_maf.R
@@ -4,10 +4,13 @@
 #     mutate(mutation = str_c('p.', ref, aa_position, var)) %>% select(Hugo_Symbol = gene, HGVSp_Short = mutation) %>% mutate(Hotspot = TRUE)
 # write.table(hotspots, file = 'data/hotspots.txt', col.names = T, row.names = F, sep = '\t', quote = F)
 
-### Also add coding mutations
+### Also add coding and truncating mutations
 # coding_mutations = c('Frame_Shift_Del', 'Frame_Shift_Ins', 'In_Frame_Del', 'In_Frame_Ins', 'Missense_Mutation',
 #                      'Nonsense_Mutation', 'Nonstop_Mutation', 'Splice_Site', 'Splice_Region', 'Targeted_Region')
 # save(coding_mutations, file = 'data/coding_mutations.rda')
+# truncating_mutations = c('Nonsense_Mutation', 'Frame_Shift_Ins', 'Frame_Shift_Del',
+#                          'Splice_Site', 'Nonstop_Mutation', 'Splice_Region')
+# save(truncating_mutations, file = 'data/truncating_mutations.rda')
 
 add_hotspots_to_maf = function(maf, hotspots = NULL) {
     if (is.null(hotspots)) data(hotspots)
