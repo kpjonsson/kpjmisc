fec429970f0951d829339f434b91674d8ce3cbb8
Philip Jonsson <kpjonsson@users.noreply.github.com>
2017-06-06 16:24:20 -0400

    Update theme_pub.R

--- R/theme_pub.R
+++ R/theme_pub.R
@@ -5,10 +5,13 @@
 library(ggplot2)
 
 theme_pub = theme_bwmin() +
-    theme(text = element_text(family = 'Helvetica', size = 6),
-          axis.text = element_text(family = 'Helvetica', size = 6, color = 'black'),
-          line = element_line(size = .25, lineend = 'round'),
-          panel.border = element_blank(),
-          axis.line = element_line(color = 'black', size = .25),
-          axis.ticks = element_line(size = .25),
-          legend.title = element_text(face = 'bold'))
+    theme(
+        text = element_text(family = 'Arial', size = 6),
+        axis.text = element_text(family = 'Arial', size = 6, color = 'black'),
+        line = element_line(size = .25, lineend = 'round'),
+        panel.border = element_blank(),
+        axis.line = element_line(color = 'black', size = .25),
+        axis.ticks = element_line(size = .25),
+        legend.title = element_text(face = 'bold'),
+        panel.background = element_blank()
+    )
