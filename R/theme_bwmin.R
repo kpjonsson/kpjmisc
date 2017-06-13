dac694a2ea2ef24e21043504178c29b5ac8e9217
Philip Jonsson <kpjonsson@users.noreply.github.com>
2017-06-13 09:36:07 -0400

    Create theme_bwmin.R

--- R/theme_bwmin.R
+++ R/theme_bwmin.R
@@ -4,13 +4,9 @@
 
 library(ggplot2)
 
-theme_bwmin = function(
-    base_size = 12,
-    base_family = 'Helvetica',
-    ticks = TRUE)
-{
-  ret = theme_bw(base_size = base_size, base_family = base_family) +
+theme_bwmin = theme_bw() +
     theme(
+        text = element_text(family = 'Helvetica', size = 12),
         plot.background = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -22,7 +18,27 @@ theme_bwmin = function(
         panel.border = element_rect(fill = NA, colour = 'black', size = 0.5),
         axis.ticks = element_line(size = 0.5),
         panel.background = element_blank()
-        )
-  if (!ticks) ret = ret + theme(axis.ticks = element_blank())
-  ret
-}
+    )
+
+# theme_bwmin = function(
+#     base_size = 12,
+#     base_family = 'Helvetica',
+#     ticks = TRUE)
+# {
+#   ret = theme_bw(base_size = base_size, base_family = base_family) +
+#     theme(
+#         plot.background = element_blank(),
+#         panel.grid.major = element_blank(),
+#         panel.grid.minor = element_blank(),
+#         strip.background = element_blank(),
+#         axis.line = element_blank(),
+#         legend.background = element_blank(),
+#         legend.key = element_blank(),
+#         legend.key.size = unit(.25, "cm"),
+#         panel.border = element_rect(fill = NA, colour = 'black', size = 0.5),
+#         axis.ticks = element_line(size = 0.5),
+#         panel.background = element_blank()
+#         )
+#   if (!ticks) ret = ret + theme(axis.ticks = element_blank())
+#   ret
+# }
