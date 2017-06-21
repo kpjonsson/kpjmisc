090d73eeeaf3d21389bfc2459e7290754971bdfb
Philip Jonsson <kpjonsson@users.noreply.github.com>
2017-06-21 13:38:49 -0400

    Update read_maf.R

--- R/read_maf.R
+++ R/read_maf.R
@@ -1,7 +1,8 @@
 read_maf = function(maf_path) {
-    maf = suppressWarnings(data.table::fread(maf_path, showProgress = F))
+    maf = suppressMessages(suppressWarnings(data.table::fread(maf_path, showProgress = F, verbose = F)))
     maf_rows = nrow(maf)
     maf_samples = length(unique(maf$Tumor_Sample_Barcode))
     cat(paste0('Read MAF file with ', maf_rows, ' rows and ', maf_samples, ' samples.\n'))
-    maf
+    maf %>%
+        mutate(t_depth = t_alt_count+t_ref_count, t_var_freq = t_alt_count/t_depth)
 }
