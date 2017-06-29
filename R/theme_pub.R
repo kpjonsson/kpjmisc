efa949dcdc2e43ff2288f7a74d08710f37c17f8c
Philip Jonsson <kpjonsson@users.noreply.github.com>
2017-06-29 10:14:40 -0400

    Update theme_pub.R

--- R/theme_pub.R
+++ R/theme_pub.R
@@ -16,13 +16,13 @@ theme_pub = theme_bw() +
         panel.grid.minor.y = element_blank(),
         strip.background = element_blank(),
         panel.border = element_blank(),
-        axis.line.x = element_line(color = 'black', size = .75),
-        axis.line.y = element_line(color = 'black', size = .75),
+        axis.line.x = element_line(color = 'black', size = .75/0.352777778),
+        axis.line.y = element_line(color = 'black', size = .75/0.352777778),
         axis.ticks.length = unit(2, 'pt'),
         legend.background = element_blank(),
         legend.key = element_blank(),
         legend.key.size = unit(.25, "cm"),
-        axis.ticks = element_line(size = .75),
+        axis.ticks = element_line(size = .75/0.352777778),
         legend.title = element_text(face = 'bold'),
         panel.background = element_blank()
     )
