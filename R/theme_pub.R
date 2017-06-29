b1138a3b3ec83f0efe152e8b715422e9e0e54336
Philip Jonsson <kpjonsson@users.noreply.github.com>
2017-06-29 10:41:01 -0400

    Update theme_pub.R

--- R/theme_pub.R
+++ R/theme_pub.R
@@ -8,7 +8,7 @@ theme_pub = theme_bw() +
     theme(
         text = element_text(family = 'ArialMT', size = 6),
         axis.text = element_text(family = 'ArialMT', size = 6, color = 'black'),
-        line = element_line(size = .75, lineend = 'round'),
+        line = element_line(size = .75*0.352777778, lineend = 'round'),
         plot.background = element_blank(),
         panel.grid.major.x = element_blank(),
         panel.grid.major.y = element_blank(),
@@ -16,13 +16,13 @@ theme_pub = theme_bw() +
         panel.grid.minor.y = element_blank(),
         strip.background = element_blank(),
         panel.border = element_blank(),
-        axis.line.x = element_line(color = 'black', size = .75),
-        axis.line.y = element_line(color = 'black', size = .75),
+        axis.line.x = element_line(color = 'black', size = .75*0.352777778),
+        axis.line.y = element_line(color = 'black', size = .75*0.352777778),
         axis.ticks.length = unit(2, 'pt'),
         legend.background = element_blank(),
         legend.key = element_blank(),
         legend.key.size = unit(.25, "cm"),
-        axis.ticks = element_line(color = 'black', size = .75),
+        axis.ticks = element_line(color = 'black', size = .75*0.352777778),
         legend.title = element_text(face = 'bold'),
         panel.background = element_blank()
     )
