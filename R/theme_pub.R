067283b2a88cd7a347e4aef35e847669a5f66122
Philip Jonsson <kpjonsson@users.noreply.github.com>
2017-07-11 21:46:40 -0400

    Update theme_pub.R

--- R/theme_pub.R
+++ R/theme_pub.R
@@ -6,8 +6,8 @@ library(ggplot2)
 
 theme_pub = theme_bw() +
     theme(
-        text = element_text(family = 'ArialMT', size = 8),
-        axis.text = element_text(family = 'ArialMT', size = 8, color = 'black'),
+        text = element_text(family = 'ArialMT', size = 6),
+        axis.text = element_text(family = 'ArialMT', size = 6, color = 'black'),
         line = element_line(size = .75*0.352777778, lineend = 'round'),
         plot.background = element_blank(),
         panel.grid.major.x = element_blank(),
