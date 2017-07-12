06a0533bbe9f6b530535d6ab5186d63664bb8343
Philip Jonsson <kpjonsson@users.noreply.github.com>
2017-07-11 21:45:41 -0400

    Update theme_pub.R

--- R/theme_pub.R
+++ R/theme_pub.R
@@ -6,8 +6,8 @@ library(ggplot2)
 
 theme_pub = theme_bw() +
     theme(
-        text = element_text(family = 'ArialMT', size = 6),
-        axis.text = element_text(family = 'ArialMT', size = 6, color = 'black'),
+        text = element_text(family = 'ArialMT', size = 8),
+        axis.text = element_text(family = 'ArialMT', size = 8, color = 'black'),
         line = element_line(size = .75*0.352777778, lineend = 'round'),
         plot.background = element_blank(),
         panel.grid.major.x = element_blank(),
@@ -19,10 +19,11 @@ theme_pub = theme_bw() +
         axis.line.x = element_line(color = 'black', size = .75*0.352777778, lineend = 'round'),
         axis.line.y = element_line(color = 'black', size = .75*0.352777778, lineend = 'round'),
         axis.ticks.length = unit(2, 'pt'),
+        axis.ticks.x = element_line(color = 'black', size = .75*0.352777778, lineend = 'round'),
+        axis.ticks.y = element_line(color = 'black', size = .75*0.352777778, lineend = 'round'),
         legend.background = element_blank(),
         legend.key = element_blank(),
         legend.key.size = unit(.25, "cm"),
-        axis.ticks = element_line(color = 'black', size = .75*0.352777778, lineend = 'round'),
         legend.title = element_text(face = 'bold'),
         panel.background = element_blank(),
         plot.margin = unit(c(0.25,0.5,0.25,0.25), 'lines')
