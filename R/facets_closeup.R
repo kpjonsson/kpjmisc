eed97e10fefe2023afe5657692c183d3d84b074f
kpjonsson <philip.jonsson@gmail.com>
2017-11-15 09:01:44 -0500

    Fix, additions

---
+++ R/facets_closeup.R
@@ -0,0 +1,29 @@
+source('/luna/git/facets-suite/fPlots_ggplot2.R')
+
+chroms = list('BRCA1' = 16:18,
+              'BRCA2' = 12:14)
+
+### Function to do gene-level
+facets_closeup = function(path,
+                          gene,
+                          chrom_range = NULL) {
+
+    ### Load Rdata object
+    load(path)
+
+    ### Which chromosomes?
+    if (!is.null(chrom_range)) chrom_range = chroms[[gene]]
+
+    ### Call close-up function
+    gene_cu = close.up(out, fit,
+                       gene.name = gene,
+                       chrom.range = chrom_range,
+                       subset.snps = F,
+                       bed.path = '/luna/git/facets-suite/Homo_sapiens.GRCh37.75.canonical_exons.bed',
+                       col.1 = '#6baed6', col.2 = '#969696')
+
+    egg::ggarrange(gene_cu$cnlr,
+                   gene_cu$valor,
+                   gene_cu$icnem,
+                   ncol = 1)
+}
