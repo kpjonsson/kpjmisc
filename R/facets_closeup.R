b0021365185944c30f03731ab1fc87f2bf4efd10
kpjonsson <philip.jonsson@gmail.com>
2017-11-15 12:56:53 -0500

    fd

--- R/facets_closeup.R
+++ R/facets_closeup.R
@@ -1,17 +1,17 @@
-source('/luna/git/facets-suite/fPlots_ggplot2.R')
-
-chroms = list('BRCA1' = 16:18,
-              'BRCA2' = 12:14)
-
 ### Function to do gene-level
 facets_closeup = function(path,
                           gene,
                           chrom_range = NULL) {
 
+    ### Source function?
+    if (!exists('close.up')) source('/luna/git/facets-suite/fPlots_ggplot2.R')
+
     ### Load Rdata object
     load(path)
 
     ### Which chromosomes?
+    chroms = list('BRCA1' = 16:18,
+                  'BRCA2' = 12:14)
     if (!is.null(chrom_range)) chrom_range = chroms[[gene]]
 
     ### Call close-up function
