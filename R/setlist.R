da57c53645a0ff5012b7cb65b89a3e9a1cd50c05
kpjonsson <philip.jonsson@gmail.com>
2018-02-28 07:44:32 -0500

    New function: setlist.R

---
+++ R/setlist.R
@@ -0,0 +1,18 @@
+setlist = function(a, b, output = F) {
+    a = unique(a)
+    b  = unique(b)
+
+    ab = intersect(a, b)
+    a_not_b = setdiff(a, b)
+    b_not_a = setdiff(b, a)
+
+    cat(paste0(
+        'Unique items A: ', length(a), '\n',
+        'Unique items B: ', length(b), '\n',
+        'Intersect: ', length(ab), '\n',
+        'Unique to A: ', length(a_not_b), '\n',
+        'Unique to B: ', length(b_not_a), '\n'
+    ))
+
+    if(output) list('A' = a, 'B', b, 'AB' = ab, 'A_not_B' = a_not_b, 'B_not_A' = b_not_a)
+}
