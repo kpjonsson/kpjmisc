92769fd6a9ac8f526cd8c268d1cf7c038e5fd6eb
kpjonsson <philip.jonsson@gmail.com>
2018-03-11 17:14:23 -0400

    New function for hotspot annotations and oncokb annotation

---
+++ R/hotspot_annotate_maf.R
@@ -0,0 +1,79 @@
+# Source of hotspots
+hs_from_local = c('/luna/work/hotspots/24k/hotspots_24k_FULL.txt',
+                  '/luna/work/hotspots/nbt/hotspots_NBT_FULL.txt',
+                  '/luna/work/hotspots/3d/hotspots.txt')
+
+hs_from_cluster = c('/ifs/work/taylorlab/jonssonp/hotspots/24k/hotspots_24k_FULL.txt',
+                    '/ifs/work/taylorlab/jonssonp/hotspots/nbt/hotspots_NBT_FULL.txt',
+                    '/ifs/work/taylorlab/jonssonp/hotspots/3d/hotspots.txt')
+
+if (Sys.info()[['nodename']] == 'lski2423') {
+    hotspot_files =  hs_from_local
+    } else {
+    hotspot_files = hs_from_cluster
+    }
+
+hotspot_annotate_maf = function(maf, hotspots = NULL)
+{
+    if (!inherits(maf, 'data.frame')) stop('Input MAF must be a data frame, preferrable VEP annotated')
+    if (!is.null(hotspots) & !inherits(hotspots, 'data.frame')) {
+        stop('Hotspots must be a data frame with a least columns Gene and Residue')
+    }
+
+    if (any(grepl('hotspot', tolower(names(hotspots))))) message('Hotspot columns in MAF might be overwritten, check names')
+
+    # Read default hotspot lists if user does not supply
+    if (is.null(hotspots)) {
+        hotspots = map_dfr(hotspot_files, fread) %>%
+            mutate(indel_hotspot = Type == 'in-frame indel',
+                   indel_hotspot = ifelse(is.na(indel_hotspot), F, indel_hotspot),
+                   threeD_hotspot = ifelse(Class %in% c('Hotspot-linked', 'Cluster-exclusive'), T, F),
+                   snv_hotspot = indel_hotspot == F & threeD_hotspot == F,
+                   Pos = ifelse(indel_hotspot == F, str_extract(Residue, '(?<=[A-Z])[0-9]+'), NA),
+                   Start = ifelse(indel_hotspot == T, str_extract(Residue, '^[0-9]+(?=-)'), NA),
+                   End = ifelse(indel_hotspot == T, str_extract(Residue, '(?<=-)[0-9]+$'), NA),
+                   Pos = ifelse(indel_hotspot == T & is.na(Start), Residue, Pos)) %>%
+            distinct(Gene, Residue, snv_hotspot, indel_hotspot, threeD_hotspot, Pos, Start, End) %>%
+            mutate(tag = str_c(Gene, Pos),
+                   Pos = as.numeric(Pos),
+                   Start = as.numeric(Start),
+                   End = as.numeric(End))
+    }
+
+    # Function that deals with indel hotspots
+    tag_indel_hotspot = function(gene, start, end) {
+        is_hotspot = 'none'
+        if (end - start <= 5) { # if short hotspot overlapping known hotspot
+            snv_hs = filter(hotspots, Gene == gene & (indel_hotspot == F | is.na(End))) %>% # includes indel hotspots that only cover one codon
+                filter(between(Pos, start, end))
+            if (nrow(snv_hs) > 0) is_hotspot = 'novel'
+        }
+        indel_hs = filter(hotspots, Gene == gene, indel_hotspot == T) %>%
+            filter(Start <= start & End >= end)
+        if (nrow(indel_hs) > 0) is_hotspot = 'prior'
+        return(is_hotspot)
+    }
+
+    maf = mutate(maf,
+                 residue = str_extract(Protein_position, '^[0-9]+(?=/|-)'),
+                 start_residue = residue,
+                 end_residue = str_extract(Protein_position, '(?<=-)[0-9]+(?=/)'),
+                 end_residue = ifelse(is.na(end_residue) & Variant_Classification %like% 'In_Frame', start_residue, end_residue)) %>%
+        rowwise() %>%
+        mutate(
+            start_residue = as.numeric(start_residue),
+            end_residue = as.numeric(end_residue),
+            snv_hotspot = ifelse(Variant_Type == 'SNP' & Variant_Classification %in% coding_mutations & !is.na(residue),
+                                 str_c(Hugo_Symbol, residue) %in% hotspots$tag[hotspots$snv_hotspot == T],
+                                 FALSE),
+            threeD_hotspot = ifelse(Variant_Type == 'SNP' & Variant_Classification %in% coding_mutations & !is.na(residue),
+                                    str_c(Hugo_Symbol, residue) %in% hotspots$tag[hotspots$threeD_hotspot == T],
+                                    FALSE),
+            indel_hotspot_type = ifelse(Variant_Classification %like% 'In_Frame',
+                                        tag_indel_hotspot(Hugo_Symbol, start_residue, end_residue),
+                                        'none'),
+            indel_hotspot = indel_hotspot_type != 'none',
+            Hotspot = snv_hotspot == T | threeD_hotspot == T | indel_hotspot == T)
+
+    maf
+}
---
+++ R/oncokb_annotate_maf.R
@@ -0,0 +1,74 @@
+suppressPackageStartupMessages({
+    library(httr)
+})
+
+consequence_map = c('3\'Flank'= 'any',
+                    '5\'Flank '= 'any',
+                    # 'Targeted_Region'= 'inframe_deletion', 'inframe_insertion',
+                    'Frame_Shift_Del'= 'frameshift_variant',
+                    'Frame_Shift_Ins'= 'frameshift_variant',
+                    'In_Frame_Del'= 'inframe_deletion',
+                    'In_Frame_Ins'= 'inframe_insertion',
+                    'Missense_Mutation'= 'missense_variant',
+                    'Nonsense_Mutation'= 'stop_gained',
+                    'Nonstop_Mutation'= 'stop_lost',
+                    'Splice_Site'= 'splice_region_variant',
+                    'Translation_Start_Site'= 'start_lost')
+
+query_oncokb = function(gene, protein_change, variant_type, start, end, cancer_type = 'CANCER') {
+
+    base_url = 'http://oncokb.org/legacy-api/indicator.json?source=cbioportal'
+    tag = paste(gene, protein_change, cancer_type, sep = '-')
+    print(tag)
+    if (!exists('cached_entries')) cached_entries <<- vector(mode = 'list')
+
+    if (tag %nin% names(tag)) {
+        query_url = modify_url(base_url, query = list(
+            hugoSymbol = gene,
+            alteration = protein_change,
+            consequence = variant_type,
+            tumorType = cancer_type
+        ))
+
+        oncokb_response = GET(query_url)
+        oncokb_response = content(oncokb_response)
+
+        cached_entries[[tag]] = oncokb_response
+    } else {
+        oncokb_response = cached_entries[[tag]]
+    }
+
+    drugs = map(oncokb_response$treatments, 'drugs') %>%
+        map(., function(x) paste(unlist(x)))
+
+    tibble(oncogenic = as.character(oncokb_response$oncogenic),
+           oncokb_level = ifelse(is.null(oncokb_response$highestSensitiveLevel), '',
+                                  oncokb_response$highestSensitiveLevel),
+           oncokb_resistance_level = ifelse(is.null(oncokb_response$highestResistanceLevel), '',
+                                             oncokb_response$highestResistanceLevel),
+           oncokb_drugs = ifelse(length(drugs) == 0, '',
+                                 paste(unlist(drugs), collapse = ',')))
+}
+
+oncokb_annotate_maf = function(maf, cancer_types = NULL)
+{
+    if (is.null(cancer_type) & 'cancer_type' %nin% names(maf)) {
+        maf$cancer_type = 'CANCER'
+    } else {
+        maf = left_join(maf, cancer_types, by = 'Tumor_Sample_Barcode')
+    }
+
+    oncokb_cols = mutate(maf,
+           gene = Hugo_Symbol,
+           protein_change = str_replace(HGVSp_Short, 'p.', ''),
+           variant_type = revalue(Variant_Classification, consequence_map, warn_missing = F),
+           start = str_extract(Protein_position, '^[0-9]+(?=\\/|\\-)'),
+           end = str_extract(Protein_position, '(?<=-)[0-9]+(?=/)'),
+           ) %>%
+        select(gene, protein_change, variant_type, start, end, cancer_type) %>%
+        pmap_dfr(., query_oncokb)
+
+    bind_cols(maf,
+              oncokb_cols)
+
+}
