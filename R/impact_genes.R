15ebed9bab06688cf9ec6fb18d55d7a7f4bada61
kpjonsson <philip.jonsson@gmail.com>
2018-03-16 11:20:55 -0400

    Added impact gene lists.

---
+++ R/impact_genes.R
@@ -0,0 +1,18 @@
+#' Genes across versions of MSK-IMPACT
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 468 rows and 5 variables:
+#' \describe{
+#'   \item{Approved.Symbol}{Gene symbol, some deprecated}
+#'   \item{Approved.Name}{Full gene name}
+#'   \item{Num.Distinct.Exons}{Exons targeted}
+#'   \item{First.Design}{First panel version with gene}
+#'   \item{Gene.Symbol}{Official gene symbol, use this}
+#'   ...
+#' }
+#' @source \url{http://cmo.mskcc.org/cmo/resources/gene-lists/}
+'impact_genes'
+
+
