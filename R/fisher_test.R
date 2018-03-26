cb7a2aa5811ab642300bbd5eadc88df6ab5b3104
kpjonsson <philip.jonsson@gmail.com>
2018-03-26 16:54:59 -0400

    fisher.test wrapper

---
+++ R/fisher_test.R
@@ -0,0 +1,24 @@
+#' Fisher's test
+#'
+#' Wraps \code{fisher.test} and tidies the output with \code{broom}
+#'
+#' @param a,a_total,b,b_total Test groups A and B
+#' @param alternative \code{two.sided} \code{greater} or \code{less}
+#'
+#' @return Test statistics
+#'
+#' @importFrom broom tidy
+
+#' @export
+fisher_test = function(a, a_total,
+                       b, b_total,
+                       alternative = 'two.sided',
+                       conf_level = 0.95) {
+
+    test_data = matrix(c(a, a_total, b, b_total), ncol = 2)
+
+    fisher.test(test_data,
+                alternative,
+                conf.level = conf_level) %>%
+        tidy()
+}
